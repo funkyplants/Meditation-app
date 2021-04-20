71d6680405ff85e257e62ff1f412d786287065a0
tuvapersson <80885269+tuvapersson@users.noreply.github.com>
2021-04-20 18:45:29 +0200

    update 20/4

--- script.js
+++ script.js
@@ -146,147 +146,146 @@ function startMeditation() {
     nameInput.style.border = "solid 1px #FF0000";
     console.log("hej");
     nameInput.placeholder = "Please enter this field";
-    
-  }
-  else if (nameInput.value !== "") {
+  } else if (nameInput.value !== "") {
     personInfo = {
-      name: nameInput.value, 
+      name: nameInput.value,
       time: randomTime.value,
       theme: randomTheme.value,
     };
-  // else if (nameInput.value !== "") {
-  //   // object //
+    // else if (nameInput.value !== "") {
+    //   // object //
     // personInfo = {
     //   name: nameInput.value,
     //   time: randomTime.value,
     //   theme: randomTheme.value,
     // };
     // localStorage.personInfo = JSON.stringify(personInfo);
-  //   // parseInfo = JSON.parse(localStorage.personInfo);
-  //   // greeting.innerHTML = "Good Morning " + personInfo.name + "!";
+    //   // parseInfo = JSON.parse(localStorage.personInfo);
+    //   // greeting.innerHTML = "Good Morning " + personInfo.name + "!";
+    // }
+  }
+
+  // object //
+  // personInfo = {
+  //   name: document.getElementById("name").value,
+  // };
+
+  // window.addEventListener("load", loadHandler);
+
+  // oceanOption.addEventListener("select", oceanPreview);
+
+  // function oceanPreview() {
+  //   console.log("working");
   // }
+
+  // //comment
+  // var rainButton = document.querySelector(".rain");
+
+  // var oceanButton = document.querySelector(".ocean");
+  // var forestButton = document.querySelector(".forest");
+  // var fireButton = document.querySelector(".fire");
+
+  // var background = "rain";
+  // let audioPlaying = true;
+  // audio = new Audio("rain-sound.mp3");
+  // audio.play();
+
+  // rainButton.addEventListener("click", rainPlay);
+  // oceanButton.addEventListener("click", oceanPlay);
+  // forestButton.addEventListener("click", forestPlay);
+  // fireButton.addEventListener("click", firePlay);
+
+  // function rainPlay() {
+  //   background = "rain";
+  //   document.body.style.backgroundImage = "url('rain.jpeg')";
+
+  //   if (audioPlaying === true) {
+  //     audio.pause();
+  //     audio = new Audio("rain-sound.mp3");
+  //     audio.play();
+  //   } else if (audioPlaying === false) {
+  //     audio.pause();
+  //   }
+  // }
+  // function oceanPlay() {
+  //   background = "ocean";
+  //   document.body.style.backgroundImage = "url('ocean.jpeg')";
+
+  //   if (audioPlaying === true) {
+  //     audio.pause();
+  //     audio = new Audio("ocean-sound.mp3");
+  //     audio.play();
+  //   } else if (audioPlaying === false) {
+  //     audio.pause();
+  //   }
+  // }
+
+  // function forestPlay() {
+  //   background = "forest";
+  //   document.body.style.backgroundImage = "url('forest.jpeg')";
+
+  //   if (audioPlaying === true) {
+  //     audio.pause();
+  //     audio = new Audio("forest-sound.mp3");
+  //     audio.play();
+  //   } else if (audioPlaying === false) {
+  //     audio.pause();
+  //   }
+  // }
+
+  // function firePlay() {
+  //   background = "fire";
+  //   document.body.style.backgroundImage = "url('fire.jpeg')";
+
+  //   if (audioPlaying === true) {
+  //     audio.pause();
+  //     audio = new Audio("fire-sound.mp3");
+  //     audio.play();
+  //   } else if (audioPlaying === false) {
+  //     audio.pause();
+  //   }
+  // }
+
+  // // control timer button //
+  // var playElement = document.querySelector(".play");
+  // playElement.addEventListener("click", startStop);
+  // var timerPlaying = true;
+
+  // function startStop() {
+  //   if (timerPlaying === true) {
+  //     timerPlaying = false; //timer played
+  //     playElement.src = "pause-button.png";
+  //   } else if (timerPlaying === false) {
+  //     timerPlaying = true; //timer played
+  //     playElement.src = "play-button.png";
+  //   }
+  // }
+
+  // var audioElement = document.querySelector(".sound-symbol");
+  // audioElement.addEventListener("click", soundOnOff);
+
+  // function soundOnOff() {
+  //   if (audioPlaying === true) {
+  //     audioPlaying = false; //audio is now off
+  //     audioElement.src = "sound-off.png";
+  //     audio.pause();
+  //   } else if (audioPlaying === false) {
+  //     audioPlaying = true; //audio is now on
+  //     audioElement.src = "sound-on.png";
+  //     if (background === "rain") {
+  //       audio = new Audio("rain-sound.mp3");
+  //       audio.play();
+  //     } else if (background === "ocean") {
+  //       audio = new Audio("ocean-sound.mp3");
+  //       audio.play();
+  //     } else if (background === "forest") {
+  //       audio = new Audio("forest-sound.mp3");
+  //       audio.play();
+  //     } else if (background === "fire") {
+  //       audio = new Audio("fire-sound.mp3");
+  //       audio.play();
+  //     }
+  //   }
+  // }
+  // console.log(audioPlaying);
 }
-
-// object //
-// personInfo = {
-//   name: document.getElementById("name").value,
-// };
-
-// window.addEventListener("load", loadHandler);
-
-// oceanOption.addEventListener("select", oceanPreview);
-
-// function oceanPreview() {
-//   console.log("working");
-// }
-
-// //comment
-// var rainButton = document.querySelector(".rain");
-
-// var oceanButton = document.querySelector(".ocean");
-// var forestButton = document.querySelector(".forest");
-// var fireButton = document.querySelector(".fire");
-
-// var background = "rain";
-// let audioPlaying = true;
-// audio = new Audio("rain-sound.mp3");
-// audio.play();
-
-// rainButton.addEventListener("click", rainPlay);
-// oceanButton.addEventListener("click", oceanPlay);
-// forestButton.addEventListener("click", forestPlay);
-// fireButton.addEventListener("click", firePlay);
-
-// function rainPlay() {
-//   background = "rain";
-//   document.body.style.backgroundImage = "url('rain.jpeg')";
-
-//   if (audioPlaying === true) {
-//     audio.pause();
-//     audio = new Audio("rain-sound.mp3");
-//     audio.play();
-//   } else if (audioPlaying === false) {
-//     audio.pause();
-//   }
-// }
-// function oceanPlay() {
-//   background = "ocean";
-//   document.body.style.backgroundImage = "url('ocean.jpeg')";
-
-//   if (audioPlaying === true) {
-//     audio.pause();
-//     audio = new Audio("ocean-sound.mp3");
-//     audio.play();
-//   } else if (audioPlaying === false) {
-//     audio.pause();
-//   }
-// }
-
-// function forestPlay() {
-//   background = "forest";
-//   document.body.style.backgroundImage = "url('forest.jpeg')";
-
-//   if (audioPlaying === true) {
-//     audio.pause();
-//     audio = new Audio("forest-sound.mp3");
-//     audio.play();
-//   } else if (audioPlaying === false) {
-//     audio.pause();
-//   }
-// }
-
-// function firePlay() {
-//   background = "fire";
-//   document.body.style.backgroundImage = "url('fire.jpeg')";
-
-//   if (audioPlaying === true) {
-//     audio.pause();
-//     audio = new Audio("fire-sound.mp3");
-//     audio.play();
-//   } else if (audioPlaying === false) {
-//     audio.pause();
-//   }
-// }
-
-// // control timer button //
-// var playElement = document.querySelector(".play");
-// playElement.addEventListener("click", startStop);
-// var timerPlaying = true;
-
-// function startStop() {
-//   if (timerPlaying === true) {
-//     timerPlaying = false; //timer played
-//     playElement.src = "pause-button.png";
-//   } else if (timerPlaying === false) {
-//     timerPlaying = true; //timer played
-//     playElement.src = "play-button.png";
-//   }
-// }
-
-// var audioElement = document.querySelector(".sound-symbol");
-// audioElement.addEventListener("click", soundOnOff);
-
-// function soundOnOff() {
-//   if (audioPlaying === true) {
-//     audioPlaying = false; //audio is now off
-//     audioElement.src = "sound-off.png";
-//     audio.pause();
-//   } else if (audioPlaying === false) {
-//     audioPlaying = true; //audio is now on
-//     audioElement.src = "sound-on.png";
-//     if (background === "rain") {
-//       audio = new Audio("rain-sound.mp3");
-//       audio.play();
-//     } else if (background === "ocean") {
-//       audio = new Audio("ocean-sound.mp3");
-//       audio.play();
-//     } else if (background === "forest") {
-//       audio = new Audio("forest-sound.mp3");
-//       audio.play();
-//     } else if (background === "fire") {
-//       audio = new Audio("fire-sound.mp3");
-//       audio.play();
-//     }
-//   }
-// }
-// console.log(audioPlaying);
