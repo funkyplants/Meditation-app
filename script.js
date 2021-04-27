819ea1c654205a44151add6fde5a1fbb1f8c3b77
tuvapersson <80885269+tuvapersson@users.noreply.github.com>
2021-04-27 18:22:05 +0200

    27/4

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
-let selectedTheme = document.querySelector("#theme");
-console.log(selectedTheme.value);
+//AUDIO
 let audio = new Audio("rain-sound.mp3");
-
 let soundBtn = document.querySelector(".sound");
 soundBtn.addEventListener("click", soundPlaying);
-let mute = true;
+let mute = true; //see if audio is playing or not
+
+//Feeling lucky?
 let starImg = document.querySelector(".star-button");
 let luckyText = document.querySelector(".lucky-p");
 luckyText.addEventListener("click", randomizeSettings);
 let randomTime = document.querySelector(".time-input");
-randomTheme = document.querySelector("#theme");
+let randomTheme = document.querySelector("#theme");
 let nameInput = document.querySelector(".input-name");
 let startBtn = document.querySelector("#start-btn");
 let greeting = document.querySelector(".greeting");
+
+//time input
 let incrementTime = document.querySelector(".up-arrow");
 let decrementTime = document.querySelector(".down-arrow");
 incrementTime.addEventListener("click", increment);
 decrementTime.addEventListener("click", decrement);
 let parseTime;
 
-let personInfo;
-let parseInfo;
+//--------------GREETING------------------------//
+window.addEventListener("load", loadHandler);
+let timeBasedGreeting;
+let time;
 
-// function loadHandler() {
-//   console.log("window is loaded");
-//   if (localStorage.personInfo == undefined) {
-//     let greeting = document.querySelector(".greeting");
-//     greeting.innerHTML = "Good Morning!";
-//   } else {
-//     parseInfo = JSON.parse(localStorage.personInfo);
-//     console.log(personInfo);
-//     let greeting = document.querySelector(".greeting");
-//     greeting.innerHTML = "Good Morning " + personInfo.name + "!";
-//   }
-// }
+function loadHandler() {
+  //change greeting depending on hour
+  time = new Date().getHours();
+  console.log("window is loaded");
+  console.log(time);
+  if (time < 12) {
+    timeBasedGreeting = "Good Morning";
+  } else if (time < 18) {
+    timeBasedGreeting = "Good Day";
+  } else if (time <= 18) {
+    timeBasedGreeting = "Good Night";
+  }
+  // localStorage for the name //
+  if (localStorage.getItem("storeName") === null) {
+    greeting.innerHTML = timeBasedGreeting;
+  } else {
+    readName = localStorage.getItem("storeName");
+    greeting.innerHTML = timeBasedGreeting + " " + readName + "!";
+  }
+}
 
+// --------------- TIMER-SECTION ------------- //
 function checkNumber() {
   // isNAN = is not a number, so if "not a number" = false, it IS a number //
   if (isNaN(randomTime.value) == false) {
@@ -43,6 +56,7 @@ function checkNumber() {
     randomTime.value = parseTime + ":00";
     console.log(parseTime);
     randomTime.style.border = "solid 1px rgb(145, 145, 145)";
+    randomTime.placeholder = "";
   } else {
     console.log("not a number");
     randomTime.style.border = "solid 1px #FF0000";
@@ -52,25 +66,54 @@ function checkNumber() {
 }
 
 function increment() {
-  let parseTime = parseInt(randomTime.value);
-  console.log(parseTime);
-  randomTime.value = parseTime + 5 + ":00";
-  // if (isNaN(randomTime.value) == true) {
-  //   randomTime.value = 5 + ":00";
-  //   console.log("Hej");
-  // }
+  let parseTime;
+  if (randomTime.placeholder !== "Error") {
+    parseTime = parseInt(randomTime.value);
+    console.log(parseTime);
+    randomTime.value = parseTime + 5 + ":00";
+  } else if (randomTime.placeholder === "Error") {
+    parseTime = 5;
+    randomTime.value = parseTime + ":00";
+    randomTime.placeholder = "";
+    randomTime.style.border = "solid 1px rgb(145, 145, 145)";
+  }
+  if (parseTime % 5 !== 0) {
+    console.log("remainder");
+    let remainder = parseTime % 5;
+    //hur mycket ska vi runda upp?
+    let roundUp = 5 - remainder;
+    parseTime += roundUp;
+    randomTime.value = parseTime + ":00";
+    console.log(parseTime);
+    console.log(randomTime.value);
+  }
 }
 function decrement() {
-  let parseTime = parseInt(randomTime.value);
-  console.log(parseTime);
+  let parseTime;
+  if (randomTime.placeholder !== "Error") {
+    parseTime = parseInt(randomTime.value);
+    console.log(parseTime);
+  } else if (randomTime.placeholder === "Error") {
+    parseTime = 5;
+    randomTime.value = parseTime + ":00";
+    randomTime.placeholder = "";
+    randomTime.style.border = "solid 1px rgb(145, 145, 145)";
+  }
   if (parseTime > 5) {
     randomTime.value = parseTime - 5 + ":00";
   }
-  // if (randomTime.placeholder == "Error") {
-  //   randomTime.value = 5 + ":00";
-  // }
+  if (parseTime % 5 !== 0) {
+    console.log("remainder");
+    let remainder = parseTime % 5;
+    //hur mycket ska vi runda upp?
+    parseTime -= remainder;
+    randomTime.value = parseTime + ":00";
+    console.log(parseTime);
+    console.log(randomTime.value);
+  }
 }
 
+// ---------------- RANDOMIZATION FEATURE (STAR) ------------------- //
 function starText() {
   starImg.style.display = "none";
   luckyText.style.display = "block";
@@ -95,9 +138,9 @@ function randomizeSettings() {
   } else if (randomTheme.value === "fire") {
     firePlay();
   }
-
-  // Math.floor(Math.random() * themes.length) + 1;
 }
+
+// ------------- AUDIO --------------- //
 
 function soundPlaying() {
   if (mute === true) {
@@ -112,14 +155,14 @@ function soundPlaying() {
 }
 
 function previewFunction() {
-  console.log(selectedTheme.value);
-  if (selectedTheme.value === "rain") {
+  console.log(randomTheme.value);
+  if (randomTheme.value === "rain") {
     rainPlay();
-  } else if (selectedTheme.value === "ocean") {
+  } else if (randomTheme.value === "ocean") {
     oceanPlay();
-  } else if (selectedTheme.value === "forest") {
+  } else if (randomTheme.value === "forest") {
     forestPlay();
-  } else if (selectedTheme.value === "fire") {
+  } else if (randomTheme.value === "fire") {
     firePlay();
   }
 }
@@ -130,6 +173,7 @@ function rainPlay() {
     audio = new Audio("rain-sound.mp3");
   } else if (mute === false) {
     audio.pause();
+    audio = new Audio("rain-sound.mp3");
     audio.play();
   }
 
@@ -174,7 +218,7 @@ function firePlay() {
   document.body.style.backgroundImage = "url('fire.jpeg')";
 }
 
-// --------------------START BUTTON
+// --------------------START BUTTON---------------------- //
 
 startBtn.addEventListener("click", startMeditation);
 
@@ -183,145 +227,63 @@ function startMeditation() {
     nameInput.style.border = "solid 1px #FF0000";
     nameInput.placeholder = "Please enter this field";
   } else if (nameInput.value !== "") {
-    personInfo = {
-      name: nameInput.value,
-      time: randomTime.value,
-      theme: randomTheme.value,
-    };
-    // else if (nameInput.value !== "") {
-    //   // object //
-    // personInfo = {
-    //   name: nameInput.value,
-    //   time: randomTime.value,
-    //   theme: randomTheme.value,
-    // };
-    // localStorage.personInfo = JSON.stringify(personInfo);
-    //   // parseInfo = JSON.parse(localStorage.personInfo);
-    //   // greeting.innerHTML = "Good Morning " + personInfo.name + "!";
-    // }
+    localStorage.setItem("storeName", nameInput.value);
   }
-
-  // object //
-  // personInfo = {
-  //   name: document.getElementById("name").value,
-  // };
-
-  // window.addEventListener("load", loadHandler);
-
-  // oceanOption.addEventListener("select", oceanPreview);
-
-  // function oceanPreview() {
-  //   console.log("working");
-  // }
-
-  // //comment
-  // var rainButton = document.querySelector(".rain");
-
-  // var oceanButton = document.querySelector(".ocean");
-  // var forestButton = document.querySelector(".forest");
-  // var fireButton = document.querySelector(".fire");
-
-  // var background = "rain";
-  // let audioPlaying = true;
-  // audio = new Audio("rain-sound.mp3");
-  // audio.play();
-
-  // rainButton.addEventListener("click", rainPlay);
-  // oceanButton.addEventListener("click", oceanPlay);
-  // forestButton.addEventListener("click", forestPlay);
-  // fireButton.addEventListener("click", firePlay);
-
-  // function rainPlay() {
-  //   background = "rain";
-  //   document.body.style.backgroundImage = "url('rain.jpeg')";
-
-  //   if (audioPlaying === true) {
-  //     audio.pause();
-  //     audio = new Audio("rain-sound.mp3");
-  //     audio.play();
-  //   } else if (audioPlaying === false) {
-  //     audio.pause();
-  //   }
-  // }
-  // function oceanPlay() {
-  //   background = "ocean";
-  //   document.body.style.backgroundImage = "url('ocean.jpeg')";
-
-  //   if (audioPlaying === true) {
-  //     audio.pause();
-  //     audio = new Audio("ocean-sound.mp3");
-  //     audio.play();
-  //   } else if (audioPlaying === false) {
-  //     audio.pause();
-  //   }
-  // }
-
-  // function forestPlay() {
-  //   background = "forest";
-  //   document.body.style.backgroundImage = "url('forest.jpeg')";
-
-  //   if (audioPlaying === true) {
-  //     audio.pause();
-  //     audio = new Audio("forest-sound.mp3");
-  //     audio.play();
-  //   } else if (audioPlaying === false) {
-  //     audio.pause();
-  //   }
-  // }
-
-  // function firePlay() {
-  //   background = "fire";
-  //   document.body.style.backgroundImage = "url('fire.jpeg')";
-
-  //   if (audioPlaying === true) {
-  //     audio.pause();
-  //     audio = new Audio("fire-sound.mp3");
-  //     audio.play();
-  //   } else if (audioPlaying === false) {
-  //     audio.pause();
-  //   }
-  // }
-
-  // // control timer button //
-  // var playElement = document.querySelector(".play");
-  // playElement.addEventListener("click", startStop);
-  // var timerPlaying = true;
-
-  // function startStop() {
-  //   if (timerPlaying === true) {
-  //     timerPlaying = false; //timer played
-  //     playElement.src = "pause-button.png";
-  //   } else if (timerPlaying === false) {
-  //     timerPlaying = true; //timer played
-  //     playElement.src = "play-button.png";
-  //   }
-  // }
-
-  // var audioElement = document.querySelector(".sound-symbol");
-  // audioElement.addEventListener("click", soundOnOff);
-
-  // function soundOnOff() {
-  //   if (audioPlaying === true) {
-  //     audioPlaying = false; //audio is now off
-  //     audioElement.src = "sound-off.png";
-  //     audio.pause();
-  //   } else if (audioPlaying === false) {
-  //     audioPlaying = true; //audio is now on
-  //     audioElement.src = "sound-on.png";
-  //     if (background === "rain") {
-  //       audio = new Audio("rain-sound.mp3");
-  //       audio.play();
-  //     } else if (background === "ocean") {
-  //       audio = new Audio("ocean-sound.mp3");
-  //       audio.play();
-  //     } else if (background === "forest") {
-  //       audio = new Audio("forest-sound.mp3");
-  //       audio.play();
-  //     } else if (background === "fire") {
-  //       audio = new Audio("fire-sound.mp3");
-  //       audio.play();
-  //     }
-  //   }
-  // }
-  // console.log(audioPlaying);
 }
+
+// window.addEventListener("load", loadHandler);
+
+// if (localStorage.getItem("names") === null) {
+//   names.unshift(nameInput.value);
+//   localStorage.setItem("names", JSON.stringify(names));
+//   console.log(names);
+//   console.log(nameInput.value);
+// } else {
+//   storedNames = JSON.parse(localStorage.getItem("names"));
+//   names.unshift(nameInput.value);
+//   localStorage.setItem("names", JSON.stringify(names));
+// }
+
+// personInfo = {
+//   name: nameInput.value,
+//   time: randomTime.value,
+//   theme: randomTheme.value,
+// };
+
+// else if (nameInput.value !== "") {
+//   // object //
+// personInfo = {
+//   name: nameInput.value,
+//   time: randomTime.value,
+//   theme: randomTheme.value,
+// };
+// localStorage.personInfo = JSON.stringify(personInfo);
+//   // parseInfo = JSON.parse(localStorage.personInfo);
+//   // greeting.innerHTML = "Good Morning " + personInfo.name + "!";
+// }
+
+// object //
+// personInfo = {
+//   name: document.getElementById("name").value,
+// };
+
+// // control timer button //
+// var playElement = document.querySelector(".play");
+// playElement.addEventListener("click", startStop);
+// var timerPlaying = true;
+
+// function startStop() {
+//   if (timerPlaying === true) {
+//     timerPlaying = false; //timer played
+//     playElement.src = "pause-button.png";
+//   } else if (timerPlaying === false) {
+//     timerPlaying = true; //timer played
+//     playElement.src = "play-button.png";
+//   }
+// }
+
+// var audioElement = document.querySelector(".sound-symbol");
+// audioElement.addEventListener("click", soundOnOff);
+//   }
+// }
+// console.log(audioPlaying);
